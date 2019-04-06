b9cfad114a91103e4e64b1d8f2c4fb2007d9c307
Ito Kohei <kouhei.itou@murata.com>
2019-04-06 21:20:13 +0900

    確認用ビルド

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ca4e10c79310f51d1b7192c072be1667.js"
+  "/precache-manifest.dbb603e631545de6973895b3a9eef8fe.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "loverage"});
